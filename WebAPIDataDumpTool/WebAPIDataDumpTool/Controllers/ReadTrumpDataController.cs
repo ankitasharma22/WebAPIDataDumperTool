468d9529678ff8d2380e4c7b12f79e8e5fd941f7
ankitasharma22 <41355156+ankitasharma22@users.noreply.github.com>
2018-09-29 15:35:28 +0530

    Removed unused references

--- WebAPIDataDumpTool/WebAPIDataDumpTool/Controllers/ReadTrumpDataController.cs
+++ WebAPIDataDumpTool/WebAPIDataDumpTool/Controllers/ReadTrumpDataController.cs
@@ -1,12 +1,7 @@
-﻿using Aerospike.Client;
+using Aerospike.Client;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using WebAPIDataDumpTool.Models;
 
 namespace WebAPIDataDumpTool.Controllers
 {
